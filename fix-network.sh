2c67436ce4c3056e796d8215b480f6840416280f
safet amedov <safet@1x-193-157-201-237.uio.no>
2017-03-15 13:07:40 +0100

    ryddet i script

--- fix-network.sh
+++ fix-network.sh
@@ -1,47 +1,38 @@
 #!/bin/bash
 # Script for å sette opp statisk IP på DHCP-provisjonerte maskiner
+# Krever at ipcalc er installert for å kunne hente ut CIDR-prefiks fra netmask
 # Safet Amedov 03/2017 USIT
 #
 
-#fjern dhcp-oppsett fra interfaces-fila
-sed -i '3,6d' /etc/network/interfaces
-# sett nmcli til å manage nettverket
-sed -i 's/false/true/' /etc/NetworkManager/NetworkManager.conf
-
-
-######################################
-# Hent all viktig nettverksinfo her  #
-######################################
-# Hent gammel IP
-oldipaddr=`ifconfig | grep inet | sed 's/\:/ /' | awk 'NR==1 {print $3}'`
+primdns=129.240.2.27
+secdns=129.240.2.40
+nettkort=$(ls /sys/class/net | grep -v lo)
 # Hent IP-addresse fra DNS
 ipaddr=$(dig +short $(hostname).uio.no)
 # Hent gateway
-gateway=$(route | grep default  | awk '{print $2}')
-netmask=$(ifconfig ens160 | awk -F: '/Mask:/{print $4;}')
-prefixxx=$(ipcalc 1.2.3.4 $netmask | awk -F "[ :=]" '/Netmask:/{print $8}')
-#restart network-manager
+gateway=$(route | grep default | awk '{print $2}')
+# Hent netmask, prefiks, macadresse
+netmask=$(ifconfig $nettkort | awk -F: '/Mask:/{print $4;}')
+prefiks=$(ipcalc 1.2.3.4 $netmask | awk -F "[ :=]" '/Netmask:/{print $8}')
+macaddress=$(cat /sys/class/net/$nettkort/address)
+IPmedPrefiks=$(echo "$ipaddr/$prefiks")
+GWmedPrefiks=$(echo "$ipaddr/$prefiks")
+
+# Fjern dhcp-oppsett fra interfaces-fila
+sed -i '3,6d' /etc/network/interfaces
+# Sett nmcli til å manage nettverket
+sed -i 's/false/true/' /etc/NetworkManager/NetworkManager.conf
+
+# Restart network-manager for at endring av NetworkManager.conf skal ta effekt
 service network-manager restart
-#slå av device
+# Slå av device slik at nmcli tilkoblingen "wired connection 1" kan slettes uten
+# at det blir opprettet ny
 nmcli d disconnect ens160
-# slett gammel connection
+# Slett gammel connection
 nmcli connection delete id "Wired connection 1"
 
-ethernetnavn () {
-		ls /sys/class/net | grep -v lo
-}
 
-hostname=$(hostname)
-primdns=129.240.2.27
-secdns=129.240.2.40
-nettkort=$(ethernetnavn)
-macaddress=$(cat /sys/class/net/$nettkort/address)
-IPmedPrefiks=$(echo $ipaddr/$prefixxx)
-GWmedPrefiks=$(echo $ipaddr/$prefixxx)
-
-# Her opprettes nettverkstilkoblingen
-# 
-#
+# Her opprettes ny nettverkstilkobling
 nmcli con add con-name nettverk ifname $nettkort type ethernet ip4 ${ipmedprefiks} gw4 ${gateway}
 nmcli con mod nettverk ipv4.addresses $IPmedPrefiks
 nmcli con mod nettverk ipv4.method manual
@@ -51,5 +42,5 @@ nmcli con mod nettverk ipv4.gateway $gateway
 nmcli con mod nettverk ethernet.mac-address $macaddress
 nmcli con mod nettverk ipv4.dns-search uio.no
 nmcli d connect ens160
-#restart network
+#restart network for at endringene skal ta effekt
 systemctl restart networking
