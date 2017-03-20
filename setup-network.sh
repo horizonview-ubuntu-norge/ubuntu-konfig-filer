0ee70fcf36113aed614426c962b8632df30abcbf
safeaim <safeaim@users.noreply.github.com>
2017-03-20 09:32:28 +0100

    Endret til å slette connections etter opprettelse

--- setup-network.sh
+++ setup-network.sh
@@ -3,7 +3,6 @@
 # Krever at ipcalc er installert for å kunne hente ut CIDR-prefiks fra netmask
 # Safet Amedov 03/2017 USIT
 #
-
 primdns=129.240.2.27
 secdns=129.240.2.40
 nettkort=$(ls /sys/class/net | grep -v lo)
@@ -11,7 +10,6 @@ nettkort=$(ls /sys/class/net | grep -v lo)
 ipaddr=$(dig +short $(hostname).uio.no)
 # Hent gateway
 gateway=$(route | grep default | awk '{print $2}')
-# Hent netmask, prefiks, macadresse
 netmask=$(ifconfig $nettkort | awk -F: '/Mask:/{print $4;}')
 prefiks=$(ipcalc 1.2.3.4 $netmask | awk -F "[ :=]" '/Netmask:/{print $8}')
 macaddress=$(cat /sys/class/net/$nettkort/address)
@@ -20,20 +18,13 @@ GWmedPrefiks=$(echo "$ipaddr/$prefiks")
 
 # Fjern dhcp-oppsett fra interfaces-fila
 sed -i '3,6d' /etc/network/interfaces
-# Sett nmcli til å manage nettverket
-sed -i 's/false/true/' /etc/NetworkManager/NetworkManager.conf
 
-# Restart network-manager for at endring av NetworkManager.conf skal ta effekt
+# Restart Network-Manager
 service network-manager restart
+
 # Slå av aktiv device slik at nmcli tilkoblingen "wired connection 1" kan slettes uten
 # at det blir opprettet ny
-nmcli c
-activeconnection=$(nmcli -t -f active,uuid c | awk -F "[ :]" '/yes:/ {print $2}')
 nmcli d disconnect $(nmcli -t -f state,device d | awk -F "[ :]" '/connected:/{print $2}')
-echo $activeconnection
-# Slett aktiv connection
-nmcli con delete uuid "$activeconnection"
-
 
 # Her opprettes ny nettverkstilkobling
 nmcli con add con-name nettverk ifname $nettkort type ethernet ip4 ${IPmedPrefiks} gw4 ${gateway}
@@ -44,6 +35,6 @@ nmcli con mod nettverk ipv4.routes $GWmedPrefiks
 nmcli con mod nettverk ipv4.gateway $gateway
 nmcli con mod nettverk ethernet.mac-address $macaddress
 nmcli con mod nettverk ipv4.dns-search uio.no
-nmcli d connect ens160
-#restart network for at endringene skal ta effekt
-systemctl restart networking
+nmcli con up nettverk
+# Slett alle inaktive nettverkskort
+for i in $(nmcli -t -f active,uuid c | awk -F "[ :]" '/no:/ {print $2}'); do nmcli con delete uuid $i ;done
